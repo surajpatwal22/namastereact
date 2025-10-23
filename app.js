6ab04711f10f09c75a17d558304e90d6ae7eae57
surajpatwal22 <patwal0suraj@gmail.com>
2025-10-23 11:58:46 +0530

    1st change

---
+++ app.js
@@ -0,0 +1,13 @@
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World from react");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const heading =React.createElement("div",{id:parent},
+    [React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"h1heading"),React.createElement("h2",{},"h2 heading")]
+    ),React.createElement("div",{id:"child2"},
+        [React.createElement("h1",{},"h1heading"),React.createElement("h2",{},"h2 heading")]
+    )]
+)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(heading);
