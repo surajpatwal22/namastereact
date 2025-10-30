fa62c3ea03c01471e6886962a44a37b1fa4a834d
surajpatwal22 <patwal0suraj@gmail.com>
2025-10-30 12:03:31 +0530

    component arcetecture

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,29 @@
 import RestroCard from "./RestroCard";
 import SearchSection from "./SearchSection";
 import restaurants from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
-    
-
+    const [restaurant , setrestaurant] = useState(restaurants);
     return (
         <div className="body">
             <SearchSection />
+            <div className="restro-header">
+                    <h2>Restaurant List</h2>
+                    <div className="btn-handle">
+                    <button className="top-rated-btn" onClick={()=>{
+                        const filterrestro = restaurants.filter((restro) => restro.rating > 4.5);
+                        // console.log(filterrestro);
+                        setrestaurant(filterrestro);
+                    }}>Top Rated Restaurants</button>
+                     <button className="top-rated-btn" onClick={()=>{
+                        setrestaurant(restaurants);
+                    }}>All Restaurants</button>
+
+                    </div>
+                </div>
             <div className="restro-container">
-                {restaurants.map((restro, index) => (
+                {restaurant.map((restro, index) => (
                     <RestroCard
                         key={index}
                         name={restro.name}
